936317eaceb1129a54a04f20d851cafd823bd108
nocon <nocon@noconnoMacBook-Air.local>
2022-03-26 13:53:32 +0900

    change base

--- main.go
+++ main.go
@@ -2,16 +2,6 @@ package main
 
 import "fmt"
 
-type TestObj struct {
-	Id   int
-	Name string
-}
-
 func main() {
-	str := display("aaa")
-	println(str)
-}
-
-func display(msg string) string {
-	return fmt.Sprintf("[%v]", msg)
+	fmt.Print("aa")
 }
