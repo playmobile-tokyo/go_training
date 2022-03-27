154f17f44170e3a45cece8fc34a7f53bde976168
nocon <nocon@noconnoMacBook-Air.local>
2022-03-27 14:33:57 +0900

    mod

--- struct/main_test.go
+++ struct/main_test.go
@@ -1,33 +1,62 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func Test_display(t *testing.T) {
+func TestTestRepository_GetResponse(t *testing.T) {
+	type fields struct {
+		client Client
+	}
 	type args struct {
 		msg string
 	}
 	tests := []struct {
-		name string
-		args args
-		want string
+		name   string
+		fields fields
+		args   args
+		want   string
 	}{
 		// TODO: Add test cases.
 		{
-			name: "test ok",
-			args: args{msg: "string"},
-			want: "[string]",
+			name:   "client impl test",
+			fields: fields{client: ClientImple{}},
+			args: args{
+				msg: "aaa",
+			},
+			want: ">}aaa{<",
 		},
 		{
-			name: "test ng",
-			args: args{msg: "string"},
-			want: "string",
+			name:   "Test impl test",
+			fields: fields{client: TestImple{}},
+			args: args{
+				msg: "aaa",
+			},
+			want: "|=aaa=|",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := display(tt.args.msg); got != tt.want {
-				t.Errorf("display() = %v, want %v", got, tt.want)
+			rep := TestRepository{
+				client: tt.fields.client,
+			}
+			if got := rep.GetResponse(tt.args.msg); got != tt.want {
+				t.Errorf("TestRepository.GetResponse() = %v, want %v", got, tt.want)
 			}
 		})
 	}
+}
+
+type TestImple struct {
+}
+
+func (my TestImple) HttpClient(msg string) (string, error) {
+	fmt.Printf("TestImple ClientImple is : %s , %s", msg, my)
+	return fmt.Sprintf("=%v=", msg), nil
+}
+
+func (my TestImple) ResponseGen(msg string) (string, error) {
+	fmt.Printf("TestImple ResponseGen is : %s , %s", msg, my)
+	return fmt.Sprintf("|%v|", msg), nil
 }
